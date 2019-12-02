510ad8dc99bf863544b2734274b35ba302bd2a6e
Francesco Melchiori <franzmelchiori@gmail.com>
2019-12-02 12:21:18 +0100

    update sphinx integration

--- conf.py
+++ conf.py
@@ -19,6 +19,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- General configuration ------------------------------------------------
@@ -34,7 +35,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'rinoh.frontend.sphinx']
+    'sphinx_rtd_theme']
+#   'rinoh.frontend.sphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
